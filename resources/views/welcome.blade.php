9e1d5cf1b7a9faaa0338ccaf25c0942b4026774e
Aurélien SCHELCHER <a.schelcher@ubitransport.com>
2018-09-30 15:40:25 +0200

    Change homepage text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,9 @@
 
   <!-- Main component for a primary marketing message or call to action -->
   <div class="jumbotron">
-    <h1>Share articles and blog posts with your colleagues</h1>
-    <p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
-    <p>To see the difference between static and fixed top navbars, just scroll.</p>
+    <h1>Share tech articles with your colleagues</h1>
+    <p>Sometimes you want to share some interesting link with your colleagues, so you send it on Slack or by email. Great! Unfortunalty after a few hours, your link is lost under hundred of other message...</p>
+    <p>With PinBucket.io you can store all your links and add tags to them. Your colleagues can then find them easily!</p>
     <p>
       <a class="btn btn-lg btn-primary" href="{{ url('register') }}" role="button">Sign up &raquo;</a>
     </p>
