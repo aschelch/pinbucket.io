d7d2528d67d719f4730ebe365968661fc3fb448e
Aurélien SCHELCHER <a.schelcher@ubitransport.com>
2018-10-21 15:44:57 +0200

    Add members list

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
     <div class="row ">
         <div class="col-md-8">
           <h2>Your teams</h2>
-            @foreach(Auth::user()->teams as $team)
+            @foreach(Auth::user()->teams()->with('users')->get() as $team)
             <h4>{{$team->name}}</h4>
             <p>
               Team code : <strong>{{$team->token}}</strong><br/>
+              @php ($members = $team->users()->orderBy('name')->get())
+              Members ({{$members->count()}}) : {{$members->pluck('name')->implode(', ')}}
             </p>
             @endforeach
 
