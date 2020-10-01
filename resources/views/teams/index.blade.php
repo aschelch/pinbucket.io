2ecc7cf2d9569ad37a769a1dde053594f9b44168
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2020-10-01 19:27:51 +0200

    feat(ui): improve team ui

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -7,11 +7,9 @@
         <div class="col-md-12">
           <h2>Your teams</h2>
             @foreach(Auth::user()->teams()->with('users')->get() as $team)
-            <h4>{{$team->name}}</h4>
+            <h4>{{$team->name}} <small>(Code : {{$team->token}})</small></h4>
             <p>
-              Team code : <strong>{{$team->token}}</strong><br/>
               @php ($members = $team->users()->orderBy('created_at')->get())
-              Members ({{$members->count()}}) :<br/>
               @foreach($members as $member)
                 <img class="user-picture" src="{{ Gravatar::src($member->email, 40) }}" title="{{$member->name}}">
               @endforeach
