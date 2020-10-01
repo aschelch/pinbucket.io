d85cda3f873a5f3ad5f04a9bf0a65d37bfb2b6fa
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2020-10-01 19:21:41 +0200

    fix(ui): fix teams page

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <div class="row ">
-        <div class="col-md-8">
+        <div class="col-md-12">
           <h2>Your teams</h2>
             @foreach(Auth::user()->teams()->with('users')->get() as $team)
             <h4>{{$team->name}}</h4>
@@ -13,7 +13,7 @@
               @php ($members = $team->users()->orderBy('name')->get())
               Members ({{$members->count()}}) :<br/>
               @foreach($members as $member)
-                <img class="user-picture" src="{{ Gravatar::src($member->email, 25) }}" title="{{$member->name}}"> {{$member->name}}
+                <img class="user-picture" src="{{ Gravatar::src($member->email, 40) }}" title="{{$member->name}}">
               @endforeach
             </p>
             @endforeach
