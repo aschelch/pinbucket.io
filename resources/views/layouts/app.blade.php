a7aac2d93bb20cfed8bf35858c9b1e07305f3fb6
Aurélien Schelcher <aurelien.schelcher@gmail.com>
2020-10-02 23:20:45 +0200

    fix(asset): add asset cache busting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
     <!-- Scripts -->
     @stack('head')
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,3 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js');
+
+if (mix.inProduction()) {
+   mix.version();
+}
